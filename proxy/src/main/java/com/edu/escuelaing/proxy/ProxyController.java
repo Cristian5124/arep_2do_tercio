38fb00e58bd910b0baf69b75ece7c03b91e0d667
unknown <cristian.polo-g@labinfo.is.escuelaing.edu.co>
2025-10-21 10:28:35 -0500

    Controlador de Proxy terminado

--- proxy/src/main/java/com/edu/escuelaing/proxy/ProxyController.java
+++ proxy/src/main/java/com/edu/escuelaing/proxy/ProxyController.java
@@ -10,4 +10,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProxyController {
+
+  private String server1;
+  private String server2;
+  private int current = 0;
+
+  public ProxyController() {
+    server1 = System.getenv("MATH_SERVICE_1");
+    server2 = System.getenv("MATH_SERVICE_2");
+
+    if (server1 == null) server1 = "http://localhost:8081";
+    if (server2 == null) server2 = "http://localhost:8082";
+  }
+
+  @PostMapping("/lucassec")
+  public String lucassec(@RequestParam String value) {
+    String server = (current % 2 == 0) ? server1 : server2;
+    current++;
+
+    try {
+      URL url = new URL(server + "/lucassec?value=" + value);
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("POST");
+
+      BufferedReader in = new BufferedReader(
+        new InputStreamReader(con.getInputStream())
+      );
+      String line;
+      String result = "";
+      while ((line = in.readLine()) != null) {
+        result += line;
+      }
+      in.close();
+      return result;
+    } catch (Exception e) {
+      return "{\"status\":\"error\",\"result\":\"Error\"}";
+    }
+  }
 }
