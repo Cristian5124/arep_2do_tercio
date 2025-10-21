6565c75778b9ec4de793223400c8cb9aba9365bb
unknown <cristian.polo-g@labinfo.is.escuelaing.edu.co>
2025-10-21 10:12:55 -0500

    Creación de estructura del server proxy

---
+++ proxy/src/main/java/com/edu/escuelaing/proxy/ProxyController.java
@@ -0,0 +1,13 @@
+package com.edu.escuelaing.proxy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProxyController {
+}
